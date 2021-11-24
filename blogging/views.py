45d52831609d3b2d14aba4bf218578cad405a048
Matthew Poehler <mdpoehle@uw.edu>
2021-11-24 06:56:17 -0800

    Final assignment 08 commit

--- blogging/views.py
+++ blogging/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
 def stub_view(request, *args, **kwargs):
@@ -15,18 +17,20 @@ def stub_view(request, *args, **kwargs):
     return HttpResponse(body, content_type="text/plain")
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'list.html', context)
+class BlogListView(ListView):
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = 'list.html'
 
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+
+    def post(self, request, post_id):
+        published = Post.objects.exclude(published_date__exact=None)
+        try:
+            post = published.get(pk=post_id)
+        except Post.DoesNotExist:
+            raise Http404
+        context = {'post': post}
+        return render(request, 'blogging/detail.html', context)
